31367e9eb3c1a63de8a18690c38745a8d0169ed3
Mohsen <mohsen@desap.co.uk>
2023-11-02 08:36:55 +0330

    Initial Commit

---
+++ InnovationApp/InnovationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnovationAppApp.swift
+//  InnovationApp
+//
+//  Created by Mohsen on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct InnovationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
