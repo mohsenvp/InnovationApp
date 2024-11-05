ae69285c0889cce3cd48e5f4e5805010be35a0f7
Mohsen <77961179+mohsenvp@users.noreply.github.com>
2024-11-05 08:06:16 +0330

    Create LearningZoneLanding.swift

---
+++ InnovationApp/Components/LearningZone/LearningZoneLanding.swift
@@ -0,0 +1,23 @@
+//
+//  LearningZoneLanding.swift
+//  InnovationApp
+//
+//  Created by Mohsen on 04/12/2023.
+//
+
+import SwiftUI
+import LearningZone
+
+struct LearningZoneLanding: View {
+    var body: some View {
+//        ScrollView {
+            VStack {
+                MainLearningZone()
+            }
+//        }
+    }
+}
+
+#Preview {
+    LearningZoneLanding()
+}
