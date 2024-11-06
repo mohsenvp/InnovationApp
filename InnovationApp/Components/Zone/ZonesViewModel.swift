3fa07a6b9ec38ad232affacb9f78df9f87d2d704
Mohsen <77961179+mohsenvp@users.noreply.github.com>
2024-11-06 10:21:08 +0330

    Create ZonesViewModel.swift

---
+++ InnovationApp/Components/Zone/ZonesViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  HorizontalScrollViewVM.swift
+//  Knektu
+//
+//  Created by Helia on 10/11/23.
+//
+
+import Foundation
+import SwiftUI
+
+class ZonesViewModel: ObservableObject {
+    @Published var zones: [EachZoneModel] = []
+    
+}
+
+
+//MARK: Just for the preview
+
+struct StoriesView_Previews_sample: PreviewProvider {
+    static var previews: some View {
+        Zones(viewModel: sampleViewModel)
+    }
+    
+    static var sampleViewModel: ZonesViewModel {
+        let viewModel = ZonesViewModel()
+        viewModel.zones = [EachZoneModel(id: UUID(), title: "1", image: "OnlineTutor", destination: ContainerView()),
+                           EachZoneModel(id: UUID(), title: "2", image: "LearningZone", destination: ContainerView()),
+                           EachZoneModel(id: UUID(), title: "3", image: "CommZone", destination: ContainerView())
+        ]
+
+        return viewModel
+    }
+}
+
+
+//MARK: Use Case
+
+//struct ContentView: View {
+//    @StateObject var viewModel = StoriesViewModel()
+//
+//    var body: some View {
+//        StoriesView(viewModel: viewModel)
+//            .onAppear {
+//                viewModel.stories = [
+//                    // Add the stories you want here
+//                ]
+//            }
+//    }
+//}
