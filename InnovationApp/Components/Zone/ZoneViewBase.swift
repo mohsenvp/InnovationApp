b8545f76170abd718ddab17e169fe4670475ab6e
Mohsen <77961179+mohsenvp@users.noreply.github.com>
2024-11-06 10:21:12 +0330

    Create ZoneViewBase.swift

---
+++ InnovationApp/Components/Zone/ZoneViewBase.swift
@@ -0,0 +1,56 @@
+//
+//  ZoneBase.swift
+//  Knektu
+//
+//  Created by Helia on 10/11/23.
+//
+
+import Foundation
+import SwiftUI
+import LearningZone
+import UIComponents
+
+struct Zones: View {
+    @ObservedObject var viewModel: ZonesViewModel
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 10) {
+                ForEach(viewModel.zones) { data in
+                    Zone(eachZone: data)
+                }
+            }
+        }
+    }
+}
+
+struct Zone: View {
+    var eachZone: EachZoneModel
+    
+
+    var body: some View {
+        NavigationLink(destination: AnyView(eachZone.destination)) {
+            VStack {
+                
+                Image(eachZone.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 70, height: 70)
+    //                .clipShape(Circle())
+    //                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                    .padding(4)
+                Text(eachZone.title)
+                    .font(.poppinsRegular13)
+                    .foregroundColor(Constants.BlackMain19191A)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+            }
+//            .onTapGesture {
+//
+//                print(eachZone.title)
+//
+//            }
+        }
+
+    }
+}
